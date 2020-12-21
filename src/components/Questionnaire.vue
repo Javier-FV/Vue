d3fb5dc0fa843b9d2dcd14de9f70a204497eb2f7
tellejm <61441662+tellejm@users.noreply.github.com>
2020-12-21 14:17:26 -0500

    Create Questionnaire.vue

---
+++ src/components/Questionnaire.vue
@@ -0,0 +1,232 @@
+<template>
+
+<div class="pregresp">
+<h1 id="logo-headline"  >Cuestionario</h1>
+    <hr class="divider" />
+
+
+ <tr>  
+  <div class="pregunta">1. ¿Cómo espera que evolucionen sus ingresos en los próximos años?<br />
+  </div>
+  <div class="respuestas" >
+   
+
+
+    <input type="radio" name="preg1" value="3" /> Fuerte aumento<br />
+    <input type="radio" name="preg1" value="3" /> Ligero aumento<br />
+    <input type="radio" name="preg1" value="2" /> Estable<br />
+    <input type="radio" name="preg1" value="1" /> Suave descenso<br />
+    <input type="radio" name="preg1" value="1" /> Fuerte descenso<br />
+
+  
+    </div>
+    
+        </tr>
+   <tr>  
+  <div class="pregunta">2.¿Qué proporción de su renta dispone para destinarla a las inversiones?<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg2" value="1" /> Entre 0% y 5%<br />
+    <input type="radio" name="preg2" value="1" /> Entre 5% y 10%<br />
+    <input type="radio" name="preg2" value="2" /> Entre 10% y 15%<br />
+    <input type="radio" name="preg2" value="3" /> Entre 15% y 25%<br />
+    <input type="radio" name="preg2" value="3" /> Más del 25%<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">3. ¿Cuánto tiempo piensa tener invertido su dinero?<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg3" value="3" /> Menos de 6 meses<br />
+    <input type="radio" name="preg3" value="2" /> Entre 6 y 12 meses<br />
+    <input type="radio" name="preg3" value="2" /> Entre 1 y 2 años<br />
+    <input type="radio" name="preg3" value="1" /> Entre 2 y 5 años<br />
+    <input type="radio" name="preg3" value="1" /> Más de 5 años<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">4. Cuando invierte su dinero esperaría ver resultados...<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg4" value="3" /> Antes de 1 año<br />
+    <input type="radio" name="preg4" value="2" /> Entre 1 y 3 años<br />
+    <input type="radio" name="preg4" value="2" /> Entre 3 y 7 años<br />
+    <input type="radio" name="preg4" value="1" /> Más de 7 años<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">5. Dentro de sus expectativas de inversión se inclina más por:<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg5" value="3" /> Acciones<br />
+    <input type="radio" name="preg5" value="1" /> Renta Fija en Pesos ($)<br />
+    <input type="radio" name="preg5" value="2" /> Renta fija en Dólares (US$)<br />
+    <input type="radio" name="preg5" value="2" /> Propiedad Raíz<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">6. Invertiría en una alternativa donde tenga probabilidades de perder capital?<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg6" value="1" /> Total desacuerdo<br />
+    <input type="radio" name="preg6" value="1" /> Desacuerdo<br />
+    <input type="radio" name="preg6" value="2" /> Neutral<br />
+    <input type="radio" name="preg6" value="3" /> Deacuerdo<br />
+    <input type="radio" name="preg6" value="3" /> Total acuerdo<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">7.   Existiendo la posibilidad de mejorar la rentabilidad de sus inversiones su actitud frente al riesgo sería:<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg7" value="1" /> No aceptaría inversiones con rentabilidades que sean positivas pero inciertas<br />
+    <input type="radio" name="preg7" value="2" /> Toleraría fluctuaciones en la rentabilidad del capital, aunque nunca negativas<br />
+    <input type="radio" name="preg7" value="2" /> De tiempo en tiempo toleraría rentabilidades negativas<br />
+    <input type="radio" name="preg7" value="3" /> Toleraría perfectamente las fluctuaciones propias de los mercados.<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">8.  Cuando sus inversiones se están desvalorizando...<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg8" value="1" /> Liquida la inversión <br />
+    <input type="radio" name="preg8" value="2" /> Espera un tiempo a que reaccionen los precios y sino, vende<br />
+    <input type="radio" name="preg8" value="3" /> Espera hasta que los precios se corrijan<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">9. Cuando sus inversiones van ganando....<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg9" value="1" /> Liquida la inversión para generar utilidades<br />
+    <input type="radio" name="preg9" value="3" /> Compra más para ganar más<br />
+    <input type="radio" name="preg9" value="2" /> Espera porque la inversión es a largo plazo<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">10. Escoja la  alternativa de inversión más aceptable para usted, en función de las rentabilidades máximas y mínimas esperadas. <br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg10" value="3" /> Máxima = +55% y  Mínima = -30%<br />
+    <input type="radio" name="preg10" value="2" /> Máxima = +25% y  Mínima = -15%<br />
+    <input type="radio" name="preg10" value="2" /> Máxima = +12% y  Mínima = -2%<br />
+    <input type="radio" name="preg10" value="1" /> Máxima = +6% y  Mínima = +4%<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">11. ¿En el último año con que frecuencia ha visitado usted un casino?<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg11" value="1" /> No lo he visitado<br />
+    <input type="radio" name="preg11" value="2" /> 1 - 3 veces<br />
+    <input type="radio" name="preg11" value="2" /> 4 - 6 veces<br />
+    <input type="radio" name="preg11" value="3" /> Más de 6 veces<br />
+  </div>
+  </tr> 
+   <tr> 
+  <div class="pregunta">12. ¿Suele practicar usted deportes extremos?<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg12" value="3" /> Sí<br />
+    <input type="radio" name="preg12" value="1" /> No<br />
+  </div>
+
+  </tr> 
+   <tr> 
+
+   <div class="pregunta">13. Cuándo ha invertido, su suerte ha sido...<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg13" value="1" /> Mala<br />
+    <input type="radio" name="preg13" value="2" /> Promedio<br />
+    <input type="radio" name="preg13" value="2" /> Mejor que el promedio<br />
+    <input type="radio" name="preg13" value="3" /> Excelente<br />
+  </div>
+
+    </tr> 
+   <tr> 
+
+   <div class="pregunta">14. Si sus acciones doblan su precio 5 meses después de haberlas comprado, usted...<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg14" value="1" /> Vende todas las acciones<br />
+    <input type="radio" name="preg14" value="2" /> Vende la mitad de las acciones<br />
+    <input type="radio" name="preg14" value="2" /> No hace nada<br />
+    <input type="radio" name="preg14" value="3" /> Compro más acciones<br />
+  </div>
+
+  </tr> 
+   <tr> 
+
+   <div class="pregunta">15. La mayoría de las cosas buenas que me suceden, han pasado porque...<br />
+  </div>
+  <div class="respuestas">
+    <input type="radio" name="preg15" value="1" /> Dios me ayuda.<br />
+    <input type="radio" name="preg15" value="2" /> Estaba en el lugar correcto a la hora correcta<br />
+    <input type="radio" name="preg15" value="2" /> Explote la oportunidad cuando se presentó.<br />
+    <input type="radio" name="preg15" value="3" /> Yo las he planeado<br />
+  </div>
+   </tr> 
+
+</div>
+ 
+</template>
+
+
+
+<script>
+</script>
+
+
+
+<style>
+
+.pregresp {
+border: 1px solid #7DA5E0;
+padding: 10px;
+margin: 10px;
+font-family: Arial, Verdana, Helvetica, sans-serif;
+font-size: 15px;
+font-weight: bold;
+float: left;
+width:95%;
+
+
+}
+
+.pregunta {
+color: #7DA5E0;
+
+width:50%
+
+}
+
+.pregunta {
+margin: 4% auto;
+text-align:left
+}
+#a {
+  border-spacing: 90px;
+  border: 1px solid black;
+  
+}
+
+.respuestas {
+color: #000000;
+float: left;
+text-align:left
+
+}
+
+.respuestas {
+margin-left: 300px;  
+
+}
+
+.logo-headline {
+font-size: 1.8rem; 
+
+}
+
+
+</style>
